a5f6df76a2c4232947bf8924ec11538d2286c21a
nikolaia <nikolaia@users.noreply.github.com>
2015-05-02 01:43:04 +0200

    Add files and update gitignore to ignore .vs folder

---
+++ test/WebApplication1.Tests/ValuesControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.TestHost;
+using Microsoft.Framework.DependencyInjection;
+using System.Net;
+using Xunit;
+
+namespace WebApplication1.Tests
+{
+    public class ValuesControllerTests
+    {
+        [Fact]
+        public async void GetShouldReturnTwoValues()
+        {
+            var server = TestServer.Create(app =>
+            {
+                var env = app.ApplicationServices.GetRequiredService<IHostingEnvironment>();
+                new Startup(env).Configure(app, env);
+            }, services => services.AddMvc());
+
+            var result = await server.CreateClient().GetAsync("api/values/");
+
+            Assert.True(result.IsSuccessStatusCode);
+        }
+    }
+}
